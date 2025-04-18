5f94342b3e33f9e2c5f284207cc991800fd47b49
Gabriel <gacmartins@gmail.com>
2025-04-17 23:29:13 -0300

    base-project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AplicativoAcademico
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AplicativoAcademico
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
